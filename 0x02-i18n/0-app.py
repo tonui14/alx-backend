a5453098e90d0baab995a52b605257cbc78e0d9d
tonui14 <tonuidaisy14@gmail.com>
2024-11-05 20:41:12 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
+"""
+A basic Flask application with a single route.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Render the index.html template.
+    Returns:
+        str: Rendered HTML content.
+    """
     return render_template('index.html')
 
 if __name__ == '__main__':
