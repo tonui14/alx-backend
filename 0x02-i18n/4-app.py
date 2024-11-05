eb9fd3efc6e6d4739eed0880d3ce75c238112530
tonui14 <tonuidaisy14@gmail.com>
2024-11-05 21:16:53 +0000

    3-app.py 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-3-app.py
+4-app.py
 A Flask application with Babel and internationalization (i18n) support.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 class Config:
@@ -23,20 +23,25 @@ babel = Babel(app)
 def get_locale():
     """
     Determine the best match for the user's preferred language from the browser's
-    Accept-Language header.
+    Accept-Language header or from the 'locale' query parameter in the URL.
     Returns:
         str: The best match language code (either 'en' or 'fr').
     """
+    # Check if 'locale' parameter is in the URL and if it's a valid locale
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    # Fallback to the default behavior (browser's Accept-Language header)
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/')
 def index() -> str:
     """
-    Render the 3-index.html template.
+    Render the 4-index.html template.
     Returns:
         str: Rendered HTML content.
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
