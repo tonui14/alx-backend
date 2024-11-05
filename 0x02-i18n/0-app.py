db2c1b2ca05c66d2897e0790803d8d4581f93001
tonui14 <tonuidaisy14@gmail.com>
2024-11-05 20:48:07 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 """
-A basic Flask application with a single route.
+A basic Flask application with Babel for internationalization.
 """
 
 from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
+
+class Config:
+    """Configuration for Babel-supported languages and timezone."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+# Instantiate Babel at the module level
+babel = Babel(app)
 
 @app.route('/')
 def index() -> str:
